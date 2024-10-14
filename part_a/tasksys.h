e3161719ff17c969da48d8ad0be4ce2b2e7c5dfa
hebangwen <1925585604@qq.com>
2024-10-14 22:35:04 +0800

    [WIP] move data

--- part_a/tasksys.h
+++ part_a/tasksys.h
@@ -40,7 +40,7 @@ class threadsafe_queue {
         void wait_and_pop(T& value) {
             std::unique_lock<std::mutex> lk(mut);
             data_cond.wait(lk, [this] { return !data_queue.empty(); });
-            value = data_queue.front();
+            value = std::move(data_queue.front());
             data_queue.pop();
         }
 
